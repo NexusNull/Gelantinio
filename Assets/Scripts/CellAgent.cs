34305558cd81176f4f7bd422206fe9146d3bfa47
DenBrand <dennis.brand-r5c@rub.de>
2019-11-09 00:40:51 +0100

    Added ml-agents SDK Assets to the Project, implemented some rudimentary movement logic compatible with vectorAction for the AgentAction method. Also adjusted sprite size of cell.PNG.

---
+++ Assets/Scripts/CellAgent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class CellAgent : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction) {
+
+        Vector3 controlSignal = new Vector3(vectorAction[0],
+                                            vectorAction[1],
+                                            0f);
+        if (controlSignal.magnitude > 1) controlSignal.Normalize();
+        transform.position = transform.position + controlSignal;
+    }
+}
